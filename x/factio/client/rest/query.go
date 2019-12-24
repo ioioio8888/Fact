5f38d47ee57665935d42a4da5a03c5bffeba96d2
ioioio8888 <louislui95111@gmail.com>
2019-12-24 17:42:43 +0800

    Problem: rest route for vote is not implemented
    Solution: add rest query route(getVoteList, getVotePower)

--- x/factio/client/rest/query.go
+++ x/factio/client/rest/query.go
@@ -39,3 +39,35 @@ func getFactHandler(cliCtx context.CLIContext, storeName string) http.HandlerFun
 		rest.PostProcessResponse(w, cliCtx, res)
 	}
 }
+
+//get a list of fact that the address has been voted
+func getVoteListHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[restName]
+
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/getVoteList/%s", storeName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
+}
+
+//get the VoterPower of the address
+func getVotePowerHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		paramType := vars[restName]
+
+		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/getVotePower/%s", storeName, paramType), nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+			return
+		}
+
+		rest.PostProcessResponse(w, cliCtx, res)
+	}
+}
--- x/factio/client/rest/rest.go
+++ x/factio/client/rest/rest.go
@@ -19,5 +19,8 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, storeName string) 
 
 	r.HandleFunc(fmt.Sprintf("/%s/createfact", storeName), CreateFactHandler(cliCtx)).Methods("POST")
 	r.HandleFunc(fmt.Sprintf("/%s/editfact", storeName), EditFactHandler(cliCtx)).Methods("PUT")
+	r.HandleFunc(fmt.Sprintf("/%s/{%s}/getVoteList", storeName, restName), getVoteListHandler(cliCtx, storeName)).Methods("GET")
+
+	r.HandleFunc(fmt.Sprintf("/%s/{%s}/getVotePower", storeName, restName), getVotePowerHandler(cliCtx, storeName)).Methods("GET")
 
 }
